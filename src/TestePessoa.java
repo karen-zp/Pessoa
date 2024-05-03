765a5b1112d39b3e75b3463165ea9c0730bad714
꧁𓊈𒆜🅺🅰🆁🅴🅽𒆜𓊉꧂ <karenoliveira.lc@gmail.com>
2024-05-03 17:29:43 -0300

    Pessoa

---
+++ src/TestePessoa.java
@@ -0,0 +1,8 @@
+public class TestePessoa {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Karen";
+        pessoa.telefone = "12345678";
+        pessoa.exibeDados();
+    }
+}
